deed7822742ce3504a4013b743e3c77a4bbe8a88
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-06 03:57:23 +0000

    項目名修正

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
     <table class="table table-bordered">
         <tr>
@@ -11,15 +11,15 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
 
-    {{-- メッセージ編集ページへのリンク --}}
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    {{-- タスク編集ページへのリンク --}}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
 
-    {{-- メッセージ削除フォーム --}}
+    {{-- タスク削除フォーム --}}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger mt-2']) !!}
     {!! Form::close() !!}
